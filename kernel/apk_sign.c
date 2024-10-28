531008f3b9be3ae998a049828ee2e15cfa2ee5c8
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-10-28 09:25:54 +0800

    kernel: apk_sign: add more size/hash pairs
    this will make it support
    - backslashxx's manager
    - tiann's KSU
    - official manager
    - 5ec1cff's MKSU
    - kowx712's KowSU
    - rsuntk's RKSU
    - SukiSU-Ultra's SukiSU
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/apk_sign.c
+++ kernel/apk_sign.c
@@ -357,5 +357,12 @@ bool is_manager_apk(char *path)
 	pr_info("%s: expected size: %u, expected hash: %s\n",
 		path, expected_manager_size, expected_manager_hash);
 
-	return check_v2_signature(path, expected_manager_size, expected_manager_hash);
+	return (check_v2_signature(path, 0x363, "4359c171f32543394cbc23ef908c4bb94cad7c8087002ba164c8230948c21549") // dummy.keystore
+	|| check_v2_signature(path, expected_manager_size, expected_manager_hash)  // ksun official
+	|| check_v2_signature(path, 0x33b, "c371061b19d8c7d7d6133c6a9bafe198fa944e50c1b31c9d8daa8d7f1fc2d2d6")  // tiann/KernelSU
+	|| check_v2_signature(path, 384, "7e0c6d7278a3bb8e364e0fcba95afaf3666cf5ff3c245a3b63c8833bd0445cc4")  // 5ec1cff/KernelSU
+	|| check_v2_signature(path, 0x375, "484fcba6e6c43b1fb09700633bf2fb4758f13cb0b2f4457b80d075084b26c588")  // KOWX712/KernelSU
+	|| check_v2_signature(path, 0x396, "f415f4ed9435427e1fdf7f1fccd4dbc07b3d6b8751e4dbcec6f19671f427870b")  /* rsuntk/KernelSU - should be compliant now atleast for core functionality - 251113 */
+	|| check_v2_signature(path, 0x35c, "947ae944f3de4ed4c21a7e4f7953ecf351bfa2b36239da37a34111ad29993eef")  // SukiSU-Ultra/SukiSU-Ultra
+	);
 }
